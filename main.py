5f98a894839855b7e0b5b03b99a2656fdf1e3b73
ASEEMMUHAMMAD A R <aseem@eduport.app>
2025-01-01 15:04:18 +0530

    Feat: Added Allowed Orgins

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from typing import Annotated
 from fastapi import FastAPI, File
+from fastapi.middleware.cors import CORSMiddleware
 from util import generate_response
 from trasncription import generate_transcription_data
 
 app = FastAPI()
+allowed_orgins = [
+    "http://*.eduport.ai",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allowed_orgins=allowed_orgins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/ping")
 def ping():
