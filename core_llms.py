d12c5877f65687b211b9baa9bb237a3b816f2532
Alosh Denny <97750960+aloshdenny@users.noreply.github.com>
2025-02-17 17:26:06 +0530

    optimised search query

--- core_llms.py
+++ core_llms.py
@@ -2,10 +2,18 @@ import os
 from langchain_groq.chat_models import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "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"
 os.environ["GOOGLE_API_KEY"] = "AIzaSyBLOv_Q-Ejfrs7b6g3Eg5h3Lr8J55_BsGA"
 llama_3 = ChatGroq(
     model="llama-3.3-70b-versatile",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+gemini_1 = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
     temperature=0,
     max_tokens=None,
     timeout=None,
@@ -19,5 +27,3 @@ gemini_2 = ChatGoogleGenerativeAI(
     timeout=None,
     max_retries=2,
 )
-
-llm = gemini_2
